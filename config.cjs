cdcd606d12204cbeb16c31378277ab8a8d264889
sils81 <silassils81@gmail.com>
2025-03-24 13:44:42 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "JOEL~XMD~WZj0KrAR",
+  SESSION_ID: process.env.SESSION_ID || "JOEL~XMD~fZpuSrNN",
   PREFIX: process.env.PREFIX || '.',
 //let's add menu captions by lord joel
    BOT_NAME: process.env.BOT_NAME || "ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ ᴠ¹⁰",
